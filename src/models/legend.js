059c6496c56d9b6c2ecaa4f23377f34db0350c2c
Dustin Chang <duschang27@gmail.com>
2013-03-15 02:40:25 -0700

    set lengend max char length

--- src/models/legend.js
+++ src/models/legend.js
@@ -7,7 +7,12 @@ nv.models.legend = function() {
   var margin = {top: 5, right: 0, bottom: 5, left: 0}
     , width = 400
     , height = 20
-    , getKey = function(d) { return d.key }
+    , getKey = function(d) {
+      if (d.key.length > 20) {
+        return d.key.slice(0, 20) + "...";
+      }
+      return d.key;
+    }
     , color = nv.utils.defaultColor()
     , align = true
     , dispatch = d3.dispatch('legendClick', 'legendDblclick', 'legendMouseover', 'legendMouseout')
